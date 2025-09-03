a7a585cac8c92435bb8baabfe3d5421141ac8566
Dung Le <xdung24@gmail.com>
2025-09-04 04:38:52 +0700

    Update readme

--- README.md
+++ README.md
@@ -6,6 +6,14 @@ Notes:
 
 ## Build
 
+To build the project, install golang:
+```sh
+wget https://go.dev/dl/go1.25.0.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.25.0.linux-amd64.tar.gz
+# Add to PATH to .profile or .bashrc
+export PATH=$PATH:/usr/local/go/bin
+```
+
 To build this app, linux environment is required because linux head import
 
 ```sh
--- build.sh
+++ build.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-mkdir -p build
+mkdir -p dist
 
 echo "Building for Linux x86..."
-CGO_ENABLED=1 GOOS=linux GOARCH=386 go build -a -ldflags '-extldflags "-static"' -o build/httpbot-linux-x86
+CGO_ENABLED=1 GOOS=linux GOARCH=386 go build -a -ldflags '-extldflags "-static"' -o dist/httpbot-linux-x86
 
 echo "Building for Linux x64..."
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -a -ldflags '-extldflags "-static"' -o build/httpbot-linux-x64
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -a -ldflags '-extldflags "-static"' -o dist/httpbot-linux-x64
 
 # echo "Building for Linux ARMv7..."
-# CGO_ENABLED=1 GOOS=linux GOARCH=arm go build -a -ldflags '-extldflags "-static"' -o build/httpbot-linux-armv7
+# CGO_ENABLED=1 GOOS=linux GOARCH=arm go build -a -ldflags '-extldflags "-static"' -o dist/httpbot-linux-armv7
 
 # echo "Building for Linux ARMv8..."
-# CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build -a -ldflags '-extldflags "-static"' -o build/httpbot-linux-armv8
+# CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build -a -ldflags '-extldflags "-static"' -o dist/httpbot-linux-armv8
 
 echo "Build complete."
+
+echo "Copying dist to EvonyBot..."
+cp -r dist ../EvonyBot/httpbot
