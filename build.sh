2ff449bd175cf0a8139d84272b9778364a3cff5a
Dung Le <xdung24@gmail.com>
2025-10-24 00:41:33 +0700

    Add build.bat

--- build.sh
+++ build.sh
@@ -3,16 +3,10 @@
 mkdir -p dist
 
 echo "Building for Linux x86..."
-CGO_ENABLED=1 GOOS=linux GOARCH=386 go build -a -ldflags '-extldflags "-static"' -o dist/httpbot-linux-x86
+CGO_ENABLED=1 GOOS=linux GOARCH=386 CGO_CFLAGS="-Wno-error -include signal.h -Wno-unused-variable" go build -a -ldflags '-extldflags "-static"' -o dist/httpbot-linux-x86
 
 echo "Building for Linux x64..."
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -a -ldflags '-extldflags "-static"' -o dist/httpbot-linux-x64
-
-# echo "Building for Linux ARMv7..."
-# CGO_ENABLED=1 GOOS=linux GOARCH=arm go build -a -ldflags '-extldflags "-static"' -o dist/httpbot-linux-armv7
-
-# echo "Building for Linux ARMv8..."
-# CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go build -a -ldflags '-extldflags "-static"' -o dist/httpbot-linux-armv8
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 CGO_CFLAGS="-Wno-error -include signal.h -Wno-unused-variable" go build -a -ldflags '-extldflags "-static"' -o dist/httpbot-linux-x64
 
 echo "Build complete."
 
